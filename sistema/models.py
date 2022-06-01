c824cd8d193be2f860ab7b94a980581cce3463c2
diego-goncalves-dreossi <dididreo@hotmail.com>
2022-05-31 23:22:24 -0300

    novas tabelas
    sistema/models.py atualizado

--- sistema/models.py
+++ sistema/models.py
@@ -9,6 +9,18 @@ class Categoria(models.Model):
     def __str__(self):   
         return self.nome
 
+    # Bebidas, Hortifruti, Açougue, Limpeza, Padaria, Doces, Cereais e farináceos, Enlatados, Laticínios, Higiene Pessoal, Peixaria, Eletrônicos, Mercearia 
+
+class Fornecedor(models.Model):
+    # Marcas
+    nome = models.CharField(max_length=50)
+    cnpj = models.CharField(max_length=20)
+    usuario = models.ForeignKey(Usuario,on_delete=models.DO_NOTHING)
+    img = models.ImageField(upload_to = 'fornecedores',null=True,blank=True)
+
+    def __str__(self):   
+        return self.nome
+
 class Produto(models.Model):
     nome = models.CharField(max_length=50)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
@@ -16,22 +28,27 @@ class Produto(models.Model):
     descricao = models.TextField()
     usuario = models.ForeignKey(Usuario,on_delete=models.DO_NOTHING)
     img = models.ImageField(upload_to = 'produtos',null=True,blank=True)
+    estoque = models.IntegerField(default=0)
+    fornecedor = models.ForeignKey(Fornecedor,on_delete=models.DO_NOTHING)
 
     def __str__(self):   
         return self.nome
 
-class Mercado(models.Model):
+class Filial(models.Model):
     nome = models.CharField(max_length=50)
     endereco = models.CharField(max_length=50)
     usuario = models.ForeignKey(Usuario,on_delete=models.DO_NOTHING)
-    img = models.ImageField(upload_to = 'mercados',null=True,blank=True)
+    img = models.ImageField(upload_to = 'filiais',null=True,blank=True)
 
     def __str__(self):   
         return self.nome
 
-class FormaPagamento(models.Model):
-    forma = models.CharField(max_length=50)
-    idcartao = models.CharField(max_length=50)
 
-    def __str__(self):   
-        return f"{self.nome} {self.idcartao}"
+
+
+class Pedido(models.Model):
+    produto = models.ForeignKey(Produto,on_delete=models.DO_NOTHING)
+    fornecedor = models.ForeignKey(Fornecedor,on_delete=models.DO_NOTHING)
+    filial = models.ForeignKey(Filial,on_delete=models.DO_NOTHING)
+    pag = models.CharField(max_length=20)
+    qntnovosprods = models.IntegerField(default=1)
