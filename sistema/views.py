5ed41a391b1fff006d0eee2f9cbcdba52d7ec51f
diego-goncalves-dreossi <dididreo@hotmail.com>
2022-06-08 01:09:43 -0300

    Update views.py
    exportarCategoria

--- sistema/views.py
+++ sistema/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse, Http404
 from django.shortcuts import redirect, render
 from django.core import serializers
 from autenticacao.models import Usuario
 from pedidos.models import Pedido
 from categorias.models import Categoria
 import json
+import zipfile as z
 
 def inicio(request):
     if request.session.get('usuario'):
@@ -25,10 +26,24 @@ def exportarCategorias(request):
         usuario = Usuario.objects.get(id=request.session['usuario'])
         dados = Categoria.objects.filter(usuario=usuario)
         dados = serializers.serialize('json',dados)
+        objeto_json = json.dumps(dados, indent=4)
+        with open("./media/dados/categorias.json", "w") as saida: 
+            saida.write(objeto_json) 
+        print('Funcionou')
 
-        with open("/media/dados/categorias.json", "w") as saida: 
-            json.dump(dados, saida) 
-        pass
+        #x = open("./media/dados/categorias.json", "r")
+        #print(x.read())
+
+        #zipar
+        arqz = z.ZipFile('./media/zipados/categorias.zip', 'w', z.ZIP_DEFLATED)
+        arqz.write(filename="./media/dados/categorias.json")
+        arqz.close()
+
+        # Baixar
+        nomearq = './media/zipados/categorias.zip'.split('./')[-1]
+        resposta = FileResponse(open(nomearq,' rb'),as_attachment=True)
+        return resposta
+        return redirect('/inicio', {'usuario_logado':request.session.get('usuario')})
     except Exception as erro:
         print(erro)
         return redirect('/inicio', {'usuario_logado':request.session.get('usuario')})
