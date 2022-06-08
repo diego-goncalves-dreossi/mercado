5ede240f7cd14b455a66126338fec1438fce4820
diego-goncalves-dreossi <dididreo@hotmail.com>
2022-06-08 00:26:03 -0300

    começo json

--- sistema/urls.py
+++ sistema/urls.py
@@ -5,5 +5,11 @@ app_name = 'sistema'
 urlpatterns = [
     path('inicio/',views.inicio, name='inicio'),
     path('sobre/',views.sobre, name="sobre"),
-    path('pagjson',views.pagJSON,name='pagjson')
+    path('pagjson/',views.pagJSON,name='pagjson'),
+    path('pagjson/pagexportar',views.pagJSONExp,name='pagexportar'),
+    path('pagjson/pagexportar/categorias/',views.exportarCategorias,name='exportarcategorias'),
+    path('pagjson/pagexportar/fornecedores/',views.exportarFornecedores,name='exportarfornecedores'),
+    path('pagjson/pagexportar/produtos/',views.exportarProdutos,name='exportarprodutos'),
+    path('pagjson/pagexportar/pedidos/',views.exportarPedidos,name='exportarpedidos'),
+
 ]
--- sistema/views.py
+++ sistema/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.core import serializers
 from autenticacao.models import Usuario
 from pedidos.models import Pedido
+from categorias.models import Categoria
+import json
 
 def inicio(request):
     if request.session.get('usuario'):
@@ -13,5 +15,33 @@ def sobre(request):
         return render(request,'sobre.html',{'usuario_logado':request.session.get('usuario')})
 
 def pagJSON(request):
-    return HttpResponse('Página JSON')
+    return render(request,'paginasjson/pagjson.html',{'usuario_logado':request.session.get('usuario')})
+
+def pagJSONExp(request):
+    return render(request,'paginasjson/pagexportar.html',{'usuario_logado':request.session.get('usuario')})
+
+def exportarCategorias(request):
+    try:
+        usuario = Usuario.objects.get(id=request.session['usuario'])
+        dados = Categoria.objects.filter(usuario=usuario)
+        dados = serializers.serialize('json',dados)
+
+        with open("/media/dados/categorias.json", "w") as saida: 
+            json.dump(dados, saida) 
+        pass
+    except Exception as erro:
+        print(erro)
+        return redirect('/inicio', {'usuario_logado':request.session.get('usuario')})
+
+def exportarFornecedores(request):
+    pass
+
+def exportarProdutos(request):
+    pass
+
+def exportarPedidos(request):
+    pass
+
+
+
 
