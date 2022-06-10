ff684457de6b862ea612bc72825dc641dcbc9a6d
diego-goncalves-dreossi <dididreo@hotmail.com>
2022-06-09 23:10:58 -0300

    download json
    A ideia era cada botão fornecer o zip referente a sua categoria, mas está fornecendo toda a pasta dados.

--- sistema/views.py
+++ sistema/views.py
@@ -4,6 +4,8 @@ from django.core import serializers
 from autenticacao.models import Usuario
 from pedidos.models import Pedido
 from categorias.models import Categoria
+from fornecedores.models import Fornecedor
+from produtos.models import Produto
 import json
 from zipfile import ZipFile
 from shutil import make_archive
@@ -32,27 +34,64 @@ def exportarCategorias(request):
         usuario = Usuario.objects.get(id=request.session['usuario'])
         dados = Categoria.objects.filter(usuario=usuario)
         dados = serializers.serialize('json',dados)
-        #f = open('static/dados/categorias.json','w')
-        #json.dump(dados,f, indent=4,sort_keys=True)
-        #make_archive('static/dados/categorias.json', 'zip', 'static/dados/')
+        f = open('static/dados/categorias.json','w')
+        json.dump(dados,f, indent=4,sort_keys=True)
+        make_archive('static/dados/categorias', 'zip', 'static/dados/')
 
-        with ZipFile('static/dados/categorias.zip','w') as arq:
-            arq.write('static/dados/categorias.json')
-        print('Funcionou converter/criar json')
-        
-        return redirect('/pagjson/pagexportar', {'usuario_logado':request.session.get('usuario'),'tabela':'Categorias'})
+
+        print('Categorias')
+        return render(request,'paginasjson/pagexportar.html', {'usuario_logado':request.session.get('usuario'),'tabela':'Categorias'})
     except Exception as erro:
         print(erro)
-        return redirect('/pagjson/pagexportar', {'usuario_logado':request.session.get('usuario')})
+        return render(request,'paginasjson/pagexportar.html', {'usuario_logado':request.session.get('usuario'),'tabela':'Categorias'})
 
 def exportarFornecedores(request):
-    pass
+    try:
+        usuario = Usuario.objects.get(id=request.session['usuario'])
+        dados = Fornecedor.objects.filter(usuario=usuario)
+        dados = serializers.serialize('json',dados)
+        f = open('static/dados/fornecedores.json','w')
+        json.dump(dados,f, indent=4,sort_keys=True)
+        make_archive('static/dados/fornecedores', 'zip', 'static/dados/')
+        print('Fornecedores')
+        
+        
+        return render(request,'paginasjson/pagexportar.html', {'usuario_logado':request.session.get('usuario'),'tabela':'Fornecedores'})
+    except Exception as erro:
+        print(erro)
+        return render(request,'paginasjson/pagexportar.html', {'usuario_logado':request.session.get('usuario'),'tabela':'Fornecedores'})
 
 def exportarProdutos(request):
-    pass
+    try:
+        usuario = Usuario.objects.get(id=request.session['usuario'])
+        dados = Produto.objects.filter(usuario=usuario)
+        dados = serializers.serialize('json',dados)
+        f = open('static/dados/produtos.json','w')
+        json.dump(dados,f, indent=4,sort_keys=True)
+        make_archive('static/dados/produtos', 'zip', 'static/dados/')
+        
+        
+        print('Produtos')
+        return render(request,'paginasjson/pagexportar.html', {'usuario_logado':request.session.get('usuario'),'tabela':'Produtos'})
+    except Exception as erro:
+        print(erro)
+        return render(request,'paginasjson/pagexportar.html', {'usuario_logado':request.session.get('usuario'),'tabela':'Produtos'})
 
 def exportarPedidos(request):
-    pass
+    try:
+        usuario = Usuario.objects.get(id=request.session['usuario'])
+        dados = Pedido.objects.filter(usuario=usuario)
+        dados = serializers.serialize('json',dados)
+        f = open('static/dados/pedidos.json','w')
+        json.dump(dados,f, indent=4,sort_keys=True)
+        make_archive('static/dados/pedidos', 'zip', 'static/dados/')
+
+        
+        print('Pedidos')
+        return render(request,'paginasjson/pagexportar.html', {'usuario_logado':request.session.get('usuario'),'tabela':'Pedidos'})
+    except Exception as erro:
+        print(erro)
+        return render(request,'paginasjson/pagexportar.html', {'usuario_logado':request.session.get('usuario'),'tabela':'Pedidos'})
 
 
 
