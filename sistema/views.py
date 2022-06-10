6ef27535696a77397e83c55df67e1a860d3de971
diego-goncalves-dreossi <dididreo@hotmail.com>
2022-06-09 21:49:55 -0300

    tentativa zip 2

--- sistema/views.py
+++ sistema/views.py
@@ -5,7 +5,7 @@ from autenticacao.models import Usuario
 from pedidos.models import Pedido
 from categorias.models import Categoria
 import json
-#import zipfile as z
+from zipfile import ZipFile
 from shutil import make_archive
 
 
@@ -22,23 +22,28 @@ def pagJSON(request):
 
 def pagJSONExp(request):
     
-
     return render(request,'paginasjson/pagexportar.html',{'usuario_logado':request.session.get('usuario')})
+
+def gerarJSON(request):
+    return render(request,'paginasjson/gerar.html',{'usuario_logado':request.session.get('usuario')})
 
 def exportarCategorias(request):
     try:
         usuario = Usuario.objects.get(id=request.session['usuario'])
         dados = Categoria.objects.filter(usuario=usuario)
         dados = serializers.serialize('json',dados)
-        f = open('static/dados/categorias.json','w')
-        json.dump(dados,f, indent=4,sort_keys=True)
-        make_archive('static/dados/categorias', 'zip', 'static/dados/')
+        #f = open('static/dados/categorias.json','w')
+        #json.dump(dados,f, indent=4,sort_keys=True)
+        #make_archive('static/dados/categorias.json', 'zip', 'static/dados/')
+
+        with ZipFile('static/dados/categorias.zip','w') as arq:
+            arq.write('static/dados/categorias.json')
         print('Funcionou converter/criar json')
-       
-        return redirect('/inicio', {'usuario_logado':request.session.get('usuario')})
+        
+        return redirect('/pagjson/pagexportar', {'usuario_logado':request.session.get('usuario'),'tabela':'Categorias'})
     except Exception as erro:
         print(erro)
-        return redirect('/inicio', {'usuario_logado':request.session.get('usuario')})
+        return redirect('/pagjson/pagexportar', {'usuario_logado':request.session.get('usuario')})
 
 def exportarFornecedores(request):
     pass
